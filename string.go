8a8dec76e09739bf88629bef30c150b60c6cb8c9
Roxy <hahadaxigua@gmail.com>
2022-05-19 17:56:41 +0800

    update

--- string.go
+++ string.go
@@ -3,10 +3,23 @@ package conversion
 import "github.com/spf13/cast"
 
 func String[T ~*string](t T) string {
+	if t == nil {
+		return ""
+	}
+	return cast.ToString(t)
+}
+
+func StringOrDefault[T ~*string](t T, defaultValue string) string {
+	if t == nil {
+		return defaultValue
+	}
 	return cast.ToString(t)
 }
 
 func StringPtr[T ~string](t T) *string {
+	if t == "" {
+		return nil
+	}
 	v := cast.ToString(t)
 	return &v
 }
